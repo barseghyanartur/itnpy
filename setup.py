4c3556f47f86af9fa1037b9315f813c1dad3039d
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-12-20 23:32:01 +0100

    Add python 3.11 classifier

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     project_urls={
         "Bug Tracker": "https://github.com/barseghyanartur/itnpy/issues",
